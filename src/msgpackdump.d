633f010ad7c0ca86bec3438f8062af180c676900
Hackerpilot <briancschott@gmail.com>
2015-02-28 17:26:45 -0800

    Add comment

--- src/msgpackdump.d
+++ src/msgpackdump.d
@@ -1,3 +1,10 @@
+/**
+ * Tool for viewing MessagePack files as text.
+ * Copyright: © 2015 Economic Modeling Specialists, Intl.
+ * Authors: Brian Schott
+ * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt Boost, License 1.0)
+ */
+
 import std.stdio;
 import std.exception;
 import core.bitop : bswap;
